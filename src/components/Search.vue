e88d0c05aabdb3c8174995cae02b57c8f2ab0c87
Daniel Malmgren de Oliveira <dm@wideco.se>
2020-05-05 15:30:57 +0100

    scaffolding app, adding basic components and firsta view

---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+    <div class="mt-2">Value: {{ text }}</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>All goes here</h1>
+    <Search />
+</div>
+</template>
+
+<script>
+import Search from '@/components/Search';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+    export default {
+        components:{ 
+            Search,
+        },
+        
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
