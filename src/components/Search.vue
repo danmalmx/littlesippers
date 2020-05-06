9e0168014d764d97234702faed19225f1418b251
Daniel Malmgren de Oliveira <dm@wideco.se>
2020-05-06 09:21:09 +0100

    adding bootstrap-vue, starting to style search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>
-    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
-    <div class="mt-2">Value: {{ text }}</div>
+
+  <div class="search">
+    <b-form>
+    <b-form-input class="input" placeholder="Search for a location"></b-form-input>
+    <b-button class="SearchButton" variant="success">Find</b-button>
+        
+    </b-form>
   </div>
 </template>
 
 <script>
+
     export default {
-        
+        data() {
+            return {
+                text: '',
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+.search  {
+    height: 50vh;
+    background: black;
+}
+
+.input {
+    align-self: center;
+    width: 75%;
+    display: inline-block;
+    margin: auto;
+}
+
+.searhButton {
+    align-self: center;
+    margin-left: 1rem;
+    display: inline;
+}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
 <div>
-    <h1>All goes here</h1>
+    <h1>Navbar goes here</h1>
+    <h1>Map Goes here</h1>
     <Search />
+    <h1>Footer goes here</h1>
 </div>
 </template>
 
 <script>
 import Search from '@/components/Search';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
     export default {
         components:{ 
