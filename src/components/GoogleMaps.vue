3a397347e0da5199d8cc24af4ec2816852e57452
Daniel Malmgren de Oliveira <dm@wideco.se>
2020-05-07 15:35:43 +0100

    adding google map

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -1,39 +1,41 @@
 <template>
     <div>
-        <h1>Map goes here</h1>
         <h3 v-show="error" :class="{'danger': error}" class="animated fadeInUp faster">{{error}}<i class="fas fa-times floatRight" @click="closeErrorMessage"></i></h3>
-        <div id="map"></div>
-
+        <div id="map">Map shows here</div>
     </div>
 </template>
 
 <script>
-    export default {
+    import GoogleMapsApiLoader from 'google-maps-api-loader'
 
-    data() {
-        return {
+    export default {
+        
+        data() {
+            return {
             apiKey: 'AIzaSyBIyBiGEuyhC5Cc-_W8k654CDOD6i2XndM',
             lat: 0,
             lng: 0,
             error: '',
+            google: null,
         }
     },
 
-    mounted() {
-        this.getUsersLatLng();
-
+    async mounted() {
+        await this.initiatGoogleMaps();
+        await this.initMap();
+        // await this.getUsersLatLng();
     },
 
     methods: {
         getUsersLatLng() {
             if(navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
-                
-                position => {
+                    
+                    position => {
                     this.lat = position.coords.latitude;
-                    this.lng = position.coords.longitude
-                    console.log(position.coords.latitude)
-                    console.log(position.coords.longitude)
+                    this.lng = position.coords.longitude;
+                    console.log(position.coords.latitude);
+                    console.log(position.coords.longitude);
                 },
                 error => {
                     this.error = error.message
@@ -42,17 +44,61 @@
                 )
 
             } else {
-                let cfs = confirm('Allow browser to know your current location')
                 console.log('Your browser does not support golocation API');
             }
         },
 
         closeErrorMessage()  {
             this.error = '';
+        },
+
+        initMap() {
+            
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+            
+                position => {
+                this.lat = position.coords.latitude;
+                this.lng = position.coords.longitude;
+                console.log(position.coords.latitude);
+                console.log(position.coords.longitude);
+            },
+        error => {
+            this.error = error.message
+            console.log(error.message)
+        }
+        )
+
+        } else {
+            console.log('Your browser does not support golocation API');
+        }
+        
+        let options = {
+            center: {
+                // lat: 57.7250,
+                // lng: 12.9600,
+                lat: this.lat,
+                lng: this.lng,
+            },
+            zoom: 3,
+        }
+
+            console.log(this.lat, this.lng)
+
+            let map = new google.maps.Map(document.getElementById('map'), options);
+
+        },
+
+        async initiatGoogleMaps() {
+            const googleMapApi = await GoogleMapsApiLoader({
+            apiKey: this.apiKey
+        })
+            this.google = googleMapApi;
         }
     }
         
     }
+    
 </script>
 
 <style lang="scss" scoped>
@@ -68,6 +114,7 @@
     text-align: center;
     background: red;
     transition: all ease-in 1s;
+    color: #fff;
 }
 
 .floatRight {
@@ -77,7 +124,12 @@
     transition: color ease-in 0.25s;
 }
 .floatRight:hover {
-    color:#fff
+    color:#aaa
+}
+
+#map {
+    height: 500px;
+    width: 100%;
 }
 
 </style>
